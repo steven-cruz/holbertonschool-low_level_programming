6f8fac9a42e42d6b8c13ca41571064a1dcd744a1
steven-cruz <stevencruz.300008@gmail.com>
2019-09-19 21:52:16 +0000

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,22 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char c, m;
+	char c;
 
-c = 'a';
-m = 'A';
-
-while (c <= 'z')
-{
-putchar(c);
-c = c + 1;
-}
-while (m <= 'Z')
-{
-putchar(m);
-m = m + 1;
-}
-putchar('\n');
-return (0);
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
