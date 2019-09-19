87e6526786bd7a6230c40a45d396d643181f243d
steven-cruz <stevencruz.300008@gmail.com>
2019-09-19 21:20:01 +0000

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-  char c, m;
+char c, m;
 
-  c = 'a';
-  m = 'A';
+c = 'a';
+m = 'A';
 
-  while (c <= 'z')
-    {
-      putchar(c);
-      c = c + 1;
-    }
-  while (m <= 'Z')
-    {
-      putchar(m);
-      m = m + 1;
-    }
-  putchar('\n');
-  return (0);
+while (c <= 'z')
+{
+putchar(c);
+c = c + 1;
+}
+while (m <= 'Z')
+{
+putchar(m);
+m = m + 1;
+}
+putchar('\n');
+return (0);
 }
