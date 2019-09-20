e281dda57d63830b3ea9b19683b1c3b735e6cb3f
steven-cruz <stevencruz.300008@gmail.com>
2019-09-20 03:30:44 +0000

    00 - 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '9' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
