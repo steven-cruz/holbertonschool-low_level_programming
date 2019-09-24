2a4840e175dfa45e86c74fd4728ee3c9d8ec72d0
steven-cruz <stevencruz.300008@gmail.com>
2019-09-24 16:56:21 +0000

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+include "holberton.h"
+
+/**
+ * print_last_digit - Print the last digit of an integer.
+ * @n: Integer to extract the last digit.
+ * Return: Return the last digit of an integer.
+ */
+int print_last_digit(int n)
+{
+	int digit = n % 10;
+
+	if (n < 0)
+		digit = digit * -1;
+	_putchar(digit + 48);
+	return (digit);
+}
