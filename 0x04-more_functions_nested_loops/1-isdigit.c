f2040a397ff539314a2a3dccf45da04c1f4e70d0
steven-cruz <stevencruz.300008@gmail.com>
2019-09-26 18:46:44 +0000

    checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * isdigit - checks for a digit
+ * @c :integer to evalue
+ * return: returns 1 if c is a digit other return 0
+ */
+
+int_isdigit(int c)
+{
+	if (c >= '0' && c <= '0')
+
+		return (1);
+
+	else
+
+		return (0);
+}
