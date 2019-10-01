9c48de64395c8594114e338df8f6ef1f26386827
steven-cruz <stevencruz.300008@gmail.com>
2019-10-01 23:00:40 +0000

    function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - function that prints a string.
+ * @str: string.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
