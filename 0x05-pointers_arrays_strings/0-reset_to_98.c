7ddf16103d9b46390e81ae34d8175994780b6d41
steven-cruz <stevencruz.300008@gmail.com>
2019-10-01 15:19:12 +0000

    pointer as to int as a parameter and update its value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter and updates the value
+ * it points to to 98.
+ * @n: Pointer to integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
