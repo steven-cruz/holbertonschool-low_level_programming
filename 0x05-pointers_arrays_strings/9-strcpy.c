d2e2271d397c22aac645052973b89d12e68c59d6
steven-cruz <stevencruz.300008@gmail.com>
2019-10-02 04:55:13 +0000

    test

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Function to copy a given string
+ * @dest: First parameter given to function
+ * @src: Second parameter given to function
+ * Return: Pointer to first parameter given
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
