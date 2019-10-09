691a64aadab57eb227f77d61e796d8996cd38a8b
steven-cruz <stevencruz.300008@gmail.com>
2019-10-09 03:57:35 +0000

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - locates the first occurrence in the string
+ * @s: this the string.
+ * @accept: Set of characteres to find in s.
+ *
+ * Return: Returns a pointer to the byte in s that matches one of the
+ * bytes in accept, or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (0);
+
+}
