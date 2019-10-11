9a36dd3fe40e3a6cc0c74da7d5790bb160d52e87
steven-cruz <stevencruz.300008@gmail.com>
2019-10-11 18:53:02 +0000

    natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: integer
+ * Return: result of the sqare root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+        int result = 0;
+
+        result = _fact(n, 0);
+
+        return (result);
+}
+
+/**
+ * _fact - calculate the sqare root of a number
+ * @n: integer
+ * @factor:integer
+ * Return: result of the sqare root of n
+ */
+
+int _fact(int n, int x)
+{
+        int result = 0;
+
+        if (x * x > n)
+                return (-1);
+
+        if (x * x == n)
+                return (x);
+
+        result = _fact(n, x + 1);
+        return (result);
+}
