0eb0cb5e7669ac065900e5eadd748385710bc28c
steven-cruz <stevencruz.300008@gmail.com>
2019-10-13 18:00:30 +0000

    print the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the programm that prints the name
+ * argc: the number of arguments.
+ * argv: the array that contains the arguments.
+ * return: 0.
+ */
+
+int main (int argc, car *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
