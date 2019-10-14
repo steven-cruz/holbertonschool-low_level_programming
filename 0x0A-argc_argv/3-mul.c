6bbbbf34938d3afeb04834fbad81516cf00bb099
steven-cruz <stevencruz.300008@gmail.com>
2019-10-14 03:16:35 +0000

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,19 +3,17 @@
 
 /**
  * main - multiplies two numbers.
- * @mul: multiplication result.
- * @num1: integer number.
- * @num2: integer number.
+ * @argc: integer number.
+ * @argv: integer number.
  * Return: 0.
  */
 
 int main(int argc, char *argv[])
 {
-        if (argc != 3)
-                printf("Error\n");
+	if (argc != 3)
+		printf("Error\n");
 
-        else
-                printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
-
-        return(0);
+	else
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
