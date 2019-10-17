29002c856744d3f4118ce8be370601af08fd9543
steven-cruz <stevencruz.300008@gmail.com>
2019-10-17 03:05:09 +0000

    array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a;
 
-	a = malloc(sizeof(char * size));
+	a = malloc(sizeof(char) * size);
 
 	if (size == 0)
 		return (NULL);
