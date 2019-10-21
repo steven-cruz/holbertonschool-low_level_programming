7a9d2cad4430697a73122ffa8b57e982679577b1
steven-cruz <stevencruz.300008@gmail.com>
2019-10-21 23:23:15 +0000

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - It is a new type of structure.
+ * @name: is the name of the pet.
+ * @age: age of the pet.
+ * @owner: Pet owner.
+ */
+
 struct dog
 {
 	char *name;
