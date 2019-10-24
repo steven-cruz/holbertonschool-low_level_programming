dcc2e8e98508ab8777469cca66b44818ede7bb0f
steven-cruz <stevencruz.300008@gmail.com>
2019-10-24 00:42:29 +0000

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,9 @@
 #include "function_pointers.h"
 
 /**
- *
+ * print_name - function that prints a name.
+ * @name: string to print.
+ * @f: function pointers
  */
 
 void print_name(char *name, void (*f)(char *))
