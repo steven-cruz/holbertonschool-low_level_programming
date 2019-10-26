516c57cc0feb313bb471441fc7cc87c8ce4e1e8a
steven-cruz <stevencruz.300008@gmail.com>
2019-10-26 20:00:55 +0000

    sum the all

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,31 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct pter - pointer to print all
+ * @test: pointer to input for test
+ * @printer: pointer to function
+ */
+
+typedef struct pter
+{
+	char *test;
+	void (*printer)();
+} pt;
+
+int _putchar(char c);
+void print_int(va_list arg);
+void print_char(va_list arg);
+
+void print_float(va_list arg);
+void print_string(va_list arg);
+int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_all(const char * const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif
