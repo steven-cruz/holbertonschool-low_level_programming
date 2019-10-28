49fd974fd6d2e94602a22c709ca2a59ec23a81b8
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-28 23:09:45 +0000

    Added buffer.c

--- tmp/buffer.c
+++ tmp/buffer.c
@@ -5,5 +5,37 @@
  */
 void sendbuf(buf *toprint, char c)
 {
+	toprint->alm[toprint->pos] = c;
+	toprint->pos += 1;
+	printBuffer(toprint);
+}
 
+
+/**
+ * createBuf - Create a buffer of the size specificated
+ * @size: Length of the buffer
+ */
+buf *createBuf()
+{
+	buf *newBuffer = malloc(sizeof(buf));
+	if (!newBuffer)
+		exit(100);
+	newBuffer->alm = malloc(1024);
+	if (!(newBuffer->alm))
+	{
+		free(newBuffer)
+		exit(100);
+	}
+	return(ptr);
+}
+
+
+/**
+ * printBuffer - Print the buffer to the standart output
+ * @toprint: Pointer to the buffer
+ */
+void printBuffer(buf *toprint)
+{
+	write(1, toprint->alm, toprint->pos);
+	toprint->pos = 0;
 }
