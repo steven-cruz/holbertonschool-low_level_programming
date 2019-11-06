228a3df68e84727d4a4defca27081d4869047850
steven-cruz <stevencruz.300008@gmail.com>
2019-11-06 03:33:33 +0000

    sum of all the data of a liked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of all the data of a linked list.
+ * @head: pointer to head node.
+ * Return: Always successful.
+ */
+
+int sum_listint(listint_t *head)
+{
+	unsigned int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
