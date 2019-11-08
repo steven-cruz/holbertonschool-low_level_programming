f803c15b434ecc653a257a19770418814c37cfef
steven-cruz <stevencruz.300008@gmail.com>
2019-11-08 03:47:02 +0000

    binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -17,15 +17,15 @@ void print_binary(unsigned long int n)
 		if (n != check)
 		{
 			flag = 0;
-			_putchar(48);
+			_putchar(49);
 		}
 		else if (!flag)
 		{
-			_putchar(49);
+			_putchar(48);
 		}
 		n <<= 1;
 		c++;
 	}
 	if (flag == 1)
-		_putchar(49);
+		_putchar(48);
 }
