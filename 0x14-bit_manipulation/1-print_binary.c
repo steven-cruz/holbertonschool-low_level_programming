a8c920bc07d9296a2b1c7611cd93dbcc1f236b29
steven-cruz <stevencruz.300008@gmail.com>
2019-11-08 03:40:20 +0000

    Binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: contains the value to convert.
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int c = 0, flag = 1, check = 0;
+	unsigned int size = sizeof(n) * 8;
+
+	while (c < size)
+	{
+		check = (n << 1);
+		check >>= 1;
+		if (n != check)
+		{
+			flag = 0;
+			_putchar(48);
+		}
+		else if (!flag)
+		{
+			_putchar(49);
+		}
+		n <<= 1;
+		c++;
+	}
+	if (flag == 1)
+		_putchar(49);
+}
