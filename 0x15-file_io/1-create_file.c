f076ae29ba6fe55ec6434a52a73fa51b73ff97f0
steven-cruz <stevencruz.300008@gmail.com>
2019-11-13 04:04:48 +0000

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer to filename to be created of char const type.
+ * @text_content: string to be written into file.
+ * Return: Always successful.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, length, fdwrite;
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	length = 0;
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+	fdwrite = write(fd, text_content, length);
+	if (fdwrite == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
