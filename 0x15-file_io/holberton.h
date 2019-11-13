bc49b6865da85d8c890de06f266649d360e8e858
steven-cruz <stevencruz.300008@gmail.com>
2019-11-13 02:57:13 +0000

    header fil

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
