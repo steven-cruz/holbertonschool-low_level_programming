72bd02c0b4f5b7c5d63e5d95916fe608464a162a
steven-cruz <stevencruz.300008@gmail.com>
2019-11-16 17:33:53 +0000

    copy the content of a file in another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+
+/**
+ * errorMsg - Print error message
+ * @exitCode: exit code to stop
+ * @msg: the error message to be printed
+ * @fileName: the name of the file
+ */
+
+void errorMsg(int exitCode, const char *msg, const char *fileName)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", msg, fileName);
+	exit(exitCode);
+}
+
+/**
+ * main - C program that copied file content from one to another.
+ * @argc: size of array argv and number of command line arguments.
+ * @argv: An  array  of size argc.
+ * Return: Always successful.
+ */
+int main(int argc, char **argv)
+{
+	int fd_from, fd_to, rd, wr, bz = 1024, cl_from, cl_to;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+
+	if (fd_from == -1)
+		errorMsg(98, "Error: Can't read from file", argv[1]);
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+		errorMsg(99, "Error: Can't write to", argv[2]);
+
+	for (rd = read(fd_from, buf, bz); rd > 0; rd = read(fd_from, buf, bz))
+	{
+		wr = write(fd_to, buf, rd);
+		if (wr == -1)
+			errorMsg(99, "Error: Can't write to", argv[2]);
+	}
+	if (rd == -1)
+		errorMsg(98, "Error: Can't read from file", argv[1]);
+
+	cl_from = close(fd_from);
+
+	if (cl_from == -1)
+		errorMsg(100, "Error: Can't close fd", argv[1]);
+
+	cl_to = close(fd_to);
+
+	if (cl_to == -1)
+		errorMsg(100, "Error: Can't close fd", argv[2]);
+
+	return (0);
+}
